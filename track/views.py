4456ed9077ff99134d1039e82fb300a9c4b03cc8
Patrick Lam <prof.lam@gmail.com>
2014-12-28 17:25:13 -0500

    refactoring & renaming 1

--- track/views.py
+++ track/views.py
@@ -3,26 +3,35 @@ from track.models import HoldingTypeProportionRule, HoldingType, Holding, Accoun
 from decimal import *
 from datetime import date
 
+def calculate_raw_allocations(total, current_investments):
+    raw_allocations = [] #: according to the rules, how much of each holding (1) currently held; (2) to hold
+    remaining_amount_to_allocate = {} #: amount still to allocate, for each holding type
+    for ar in HoldingTypeProportionRule.objects.all().order_by('holding_type__name'):
+        amt = ar.percent * total / 100
+        raw_allocations.append([ar.holding_type.name, current_investments.setdefault(ar.holding_type.name, 0), amt])
+        remaining_amount_to_allocate[ar.holding_type] = remaining_amount_to_allocate.setdefault(ar.holding_type, 0) + amt
+    return (raw_allocations, remaining_amount_to_allocate)
+
 def index(request):
-    new_amount = 0
-    total = 0
+    new_amount = 0 #: amount of money to add
+    total = 0 #: total pool of money available
     if 'new_amount' in request.POST:
         new_amount = Decimal(request.POST['new_amount'])
         total = new_amount
 
-    amounts = {}
+    amounts = {} #: amount of money already in each account
     for ht in Account.objects.all():
         amounts.setdefault(ht, 0)
 
-    current_investments = {}
+    current_investments = {} #: amount of money per holding type
     for ht in HoldingType.objects.all():
         for h in Holding.objects.filter(holding_type__name=ht.name):
             current_investments[ht.name] = current_investments.setdefault(ht.name, 0) + h.amount
             amounts[h.account] += h.amount
             total += h.amount
 
-    max_amounts = {}
-    room_left = {}
+    max_amounts = {} #: max amount of money one can put in an account, or N/A
+    room_left = {} #: running total of room left in each account
     current_holdings_per_account = {}
     for a in Account.objects.all():
         if a.can_add_money:
@@ -36,12 +45,8 @@ def index(request):
                 current_holdings_per_account[a] = []
             current_holdings_per_account[a].append((ht, 0))
 
-    ideal_investments = []
-    remaining_amount_to_allocate = {}
-    for ar in HoldingTypeProportionRule.objects.all().order_by('holding_type__name'):
-        amt = ar.percent * total / 100
-        ideal_investments.append([ar.holding_type.name, current_investments.setdefault(ar.holding_type.name, 0), amt])
-        remaining_amount_to_allocate[ar.holding_type] = remaining_amount_to_allocate.setdefault(ar.holding_type, 0) + amt
+    # calculate the dollar amounts for each holding type
+    raw_allocations, remaining_amount_to_allocate = calculate_raw_allocations(total, current_investments)
 
     allocated_amounts = {}
     for lr in HoldingLocationRule.objects.all().order_by('priority'):
@@ -77,7 +82,7 @@ def index(request):
 
     context = {'new_amount' : new_amount,
                'total' : total,
-               'ideal' : ideal_investments,
+               'raw' : raw_allocations,
                'max_amounts' : max_amounts,
                'current_holdings_pa' : current_holdings_per_account}
     return render(request, 'track/index.html', context)
