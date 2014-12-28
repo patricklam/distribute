ba853981e76f74fe21ef2e4740eea573202d7b4f
Patrick Lam <prof.lam@gmail.com>
2014-12-28 14:25:36 -0500

    implement holding location rules

--- track/views.py
+++ track/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from track.models import HoldingTypeProportionRule, HoldingType, Holding, Account
+from track.models import HoldingTypeProportionRule, HoldingType, Holding, Account, HoldingLocationRule
 from decimal import *
 
 def index(request):
@@ -21,6 +21,7 @@ def index(request):
             total += h.amount
 
     max_amounts = {}
+    room_left = {}
     current_holdings_per_account = {}
     for a in Account.objects.all():
         if a.can_add_money:
@@ -28,21 +29,42 @@ def index(request):
         else:
             m = amounts[a]
         max_amounts[a] = (amounts[a], m)
+        room_left[a] = m
         for ht in Holding.objects.filter(account=a):
             if not (a in current_holdings_per_account):
                 current_holdings_per_account[a] = []
-            current_holdings_per_account[a].append(ht)
+            current_holdings_per_account[a].append((ht, 0))
 
     ideal_investments = []
-
+    remaining_amount_to_allocate = {}
     for ar in HoldingTypeProportionRule.objects.all().order_by('holding_type__name'):
         amt = ar.percent * total / 100
         ideal_investments.append([ar.holding_type.name, current_investments.setdefault(ar.holding_type.name, 0), amt])
+        remaining_amount_to_allocate[ar.holding_type] = remaining_amount_to_allocate.setdefault(ar.holding_type, 0) + amt
+
+    allocated_amounts = {}
+    for lr in HoldingLocationRule.objects.all().order_by('priority'):
+        for a in Account.objects.filter(account_type = lr.account_type):
+            remaining_amount = remaining_amount_to_allocate[lr.holding_type]
+            allocated_amounts.setdefault(a, [])
+            if (room_left[a] == 'N/A' or room_left[a] > remaining_amount):
+                allocated_amounts[a].append((lr.holding_type, remaining_amount))
+                remaining_amount_to_allocate[lr.holding_type] = 0
+                if room_left[a] != 'N/A':
+                    room_left[a] -= remaining_amount
+            else:
+                allocated_amounts[a].append((lr.holding_type, room_left[a]))
+                remaining_amount_to_allocate[lr.holding_type] -= room_left[a]
+                if room_left[a] != 'N/A':
+                    room_left[a] = 0
+
+    # todo merge allocated_amounts with current_holdings_per_account
 
     context = {'new_amount' : new_amount,
                'total' : total,
                'ideal' : ideal_investments,
                'max_amounts' : max_amounts,
-               'current_holdings_pa' : current_holdings_per_account}
+               'current_holdings_pa' : current_holdings_per_account,
+               'allocated_amounts' : allocated_amounts}
     return render(request, 'track/index.html', context)
 
