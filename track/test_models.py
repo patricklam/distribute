a1b038ec8f8e79373b228a79c60f210048a4299f
Patrick Lam <prof.lam@gmail.com>
2015-01-28 22:37:05 -0500

    complete refactoring

--- track/test_models.py
+++ track/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from track.models import HoldingType, AllocationRule
+from track.models import HoldingType, HoldingTypeProportionRule
 
 class SampleData(TestCase):
     def setUp(self):    
@@ -8,7 +8,7 @@ class SampleData(TestCase):
         tdb909 = HoldingType(name="TDB909",asset_class="bond",country="CA")
         tdb911 = HoldingType(name="TDB911",asset_class="equity",country="INTL")
 
-        cdn_bonds = AllocationRule(percent=40, holding=tdb909)
-        cdn_stock = AllocationRule(percent=20, holding=tdb900)
-        us_stock  = AllocationRule(percent=20, holding=tdb902)
-        intl_stock= AllocationRule(percent=20, holding=tdb911)
+        cdn_bonds = HoldingTypeProportionRule(percent=40, holding=tdb909)
+        cdn_stock = HoldingTypeProportionRule(percent=20, holding=tdb900)
+        us_stock  = HoldingTypeProportionRule(percent=20, holding=tdb902)
+        intl_stock= HoldingTypeProportionRule(percent=20, holding=tdb911)
